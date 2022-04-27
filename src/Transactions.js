9af96bb7921155a0103c6eac6c386d441ff7e590
varunakk <56592896+varunakk@users.noreply.github.com>
2022-04-27 12:38:22 +0530

    Update Transactions.js

--- src/Transactions.js
+++ src/Transactions.js
@@ -109,12 +109,12 @@ function Transactions(){
         const token=store.getState().token;
 
         console.log(store.getState().user);
-        const st="http://localhost:5000/users/"+store.getState().user;
+        const st="https://appbankiiits.herokuapp.com/users/"+store.getState().user;
         console.log(st)
         axios.get(st,{ headers: {"token":token} }).then((res)=>{// axios sends the http async req to end points , here we are sending to port o 4000.
         console.log("res data  ... ",res.data);
         const ac=res.data.acc
-        const st1="http://localhost:5000/transactions/"+ac;
+        const st1="https://appbankiiits.herokuapp.com/transactions/"+ac;
 
         
         axios.get(st1,{ headers: {"token":token} }).then((res)=>{
